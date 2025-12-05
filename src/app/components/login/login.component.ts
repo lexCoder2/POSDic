1d80d3b79647efe85e785f9ec7e626fa49569476
lexCoder2 <hi@iarodriguez.com>
2025-12-05 05:33:01 -0600

    fix login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
+import { TranslatePipe } from "../../pipes/translate.pipe";
 
 @Component({
   selector: "app-login",
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TranslatePipe],
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
